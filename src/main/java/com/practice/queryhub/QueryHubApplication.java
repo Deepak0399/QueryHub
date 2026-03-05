2e66d169b8582f3023fceb9948976565cb0e35bf
Deepak <deepakkumar97967@gmail.com>
2026-03-05 12:14:22 +0530

    Initial Commit

---
+++ src/main/java/com/practice/queryhub/QueryHubApplication.java
@@ -0,0 +1,13 @@
+package com.practice.queryhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/queryhub/QueryHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.queryhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
